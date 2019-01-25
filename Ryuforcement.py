b2161c80255253e4343326937357cae54636052d
Camille Gouneau <camille.gouneau@epitech.eu>
2019-01-25 14:49:34 +0100

    Added some really preliminaries functions

--- Ryuforcement.py
+++ Ryuforcement.py
@@ -2,42 +2,53 @@
 import retro
 import sys
 import time
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 
 ################################################
+#ScreenPlay Handle
 
 def rgb2gray(rgb):
 	return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 ################################################
 
-if __name__ == '__main__':
-	check_action = 0
-	yeet = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+################################################
+#Q-Learning
 
+def eps_modify(eps, eps_mult):
+	return (eps * eps_mult);
+
+def random_play(env):
+	return env.action_space.sample()
+
+def pick_action(env, eps):
+	if random.uniform(0, 1) < eps:
+		action = random_play(env);
+	else:
+		action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	return action;
+
+################################################
+#Main
+
+if __name__ == '__main__':
 	env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
 	env.reset()
 	env.render()
 
+	eps = 1;
 	while True:
+		action = pick_action(env, eps)
 
-		if check_action == 0:
-			yeet = [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0]
-			check_action = 1
-		elif check_action == 1:
-			yeet = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-			check_action = 0
+		_obs, _rew, done, _info = env.step(action)
 
-		_obs, _rew, done, _info = env.step(yeet)
-
-		_grayObs = rgb2gray(_obs)
-		plt.imshow(_grayObs[28:182], cmap = plt.get_cmap('gray'))
-		plt.show()
+		eps = eps_modify(eps, 0.999);
+		#_grayObs = rgb2gray(_obs)
+		#plt.imshow(_grayObs[28:182], cmap = plt.get_cmap('gray'))
+		#plt.show()
 
 		env.render()
-
-		time.sleep(0.01)
-
 		if done:
 			break
