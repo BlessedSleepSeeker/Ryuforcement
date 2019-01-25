2c4d8baf3156612f1d26f0ad5619dbb3fe634a49
Pierre-Eloy SYLVESTRE <pierre-eloy.sylvestre@epitech.eu>
2019-01-25 11:17:29 +0100

    Add croped screen Add RGD -> Gray screen

--- Ryuforcement.py
+++ Ryuforcement.py
@@ -3,24 +3,41 @@ import retro
 import sys
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
-def getAction(env, _obs, _info):
-	act = env.action_space.sample()
-	return act
+################################################
 
-def main():
-	env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
-	env.reset()
-	_obs = 0;
-	_info = 0;
-	while True:
-		act = getAction(env, _obs, _info)
-		_obs, _rew, done, _info = env.step(act)
-		print(act)
-		env.render()
-		if done:
-			break
-		time.sleep(0.01)
+def rgb2gray(rgb):
+	return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+################################################
 
 if __name__ == '__main__':
-	main()
+	check_action = 0
+	yeet = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	
+	env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
+	env.reset()
+	env.render()
+	
+	while True:
+
+		if check_action == 0:
+			yeet = [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0]
+			check_action = 1
+		elif check_action == 1:
+			yeet = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+			check_action = 0
+		
+		_obs, _rew, done, _info = env.step(yeet)
+
+		_grayObs = rgb2gray(_obs)
+		plt.imshow(_grayObs[28:182], cmap = plt.get_cmap('gray'))
+		plt.show()
+
+		env.render()
+
+		time.sleep(0.01)
+
+		if done:
+				break
